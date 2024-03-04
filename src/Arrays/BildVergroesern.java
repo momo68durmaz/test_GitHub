497e019aa78a109c2a3a0aebcfad12a7e1bc8779
momo68durmaz <muhammed.durmaz@hak-reutte.ac.at>
2024-03-04 09:58:42 +0100

    Kommentare filtern

---
+++ src/Arrays/BildVergroesern.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class BildVergroesern {
+	
+	
+	public static int[][] magnify( int[][] array, int factor ) {
+	    int width = array[ 0 ].length;
+	    int height = array.length;
+	    int[][] result = new int[ height * factor ][ width * factor ];
+
+	    for ( int row = 0; row < result.length; row++ ) {
+	      int[] cols = result[ row ];
+	      for ( int col = 0; col < cols.length; col++ )
+	        cols[ col ] = array[ row / factor ][ col / factor ];
+	    }
+	    return result;
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Arrays/FilterComment.java
@@ -0,0 +1,46 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilterComment {
+	
+	ArrayList<String>logBuch = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		   List<String> logEntries = new ArrayList<>();
+	       logEntries.add("1. Magnetkompass");
+	       logEntries.add("2. Geschwindigkeit der Wasserströmung");
+	       logEntries.add("3. Wetter");
+	       logEntries.add("4. Kommentar und allgemeine Beobachtungen");
+	       logEntries.add("1. Magnetkompass");
+	       logEntries.add("2. Geschwindigkeit der Wasserströmung");
+	       logEntries.add("3. Wetter");
+	       logEntries.add("4. Kommentar und allgemeine Beobachtungen");
+	       logEntries.add("1. Magnetkompass");
+	       logEntries.add("2. Geschwindigkeit der Wasserströmung");
+	       logEntries.add("3. Wetter");
+	       logEntries.add("4. Kommentar und allgemeine Beobachtungen");
+	       logEntries.add("1. Magnetkompass");
+	       logEntries.add("2. Geschwindigkeit der Wasserströmung");
+	       logEntries.add("3. Wetter");
+	       logEntries.add("4. Kommentar und allgemeine Beobachtungen");
+	       reduceToComments(logEntries);
+	 
+	       for (String comment : logEntries) {
+	           System.out.println(comment);
+	       }
+	   }
+		  public static void reduceToComments(List<String> lines) {
+			  int i = 0;
+		        while (i + 3 < lines.size()) {
+		            lines.remove(i);
+		            lines.remove(i);
+		            lines.remove(i);
+		            i++;
+		        }
+		    }
+		
+	
+}
+
