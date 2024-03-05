dd2b8bf11695cc2b2aeaa28248943f0b5b6312f7
momo68durmaz <muhammed.durmaz@hak-reutte.ac.at>
2024-03-05 09:45:17 +0100

    Methoden schreiben

--- src/musicalChairs/MusicalChair.java
+++ src/musicalChairs/MusicalChair.java
@@ -1,5 +1,50 @@
 package musicalChairs;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class MusicalChair {
+	
+	private List<String> names;
+	
+	public MusicalChair(String... names) {
+		// String... kann beliebig viele Strings übernehmen
+		// Java regelt das intern mit einem Array
+		// dadurch wird length und nicht size verwendet!
+		if(names.length > 0) {
+			// Arrays.asList(names) - wandelt ein Array in eine Liste um
+			this.names = new LinkedList<String>(Arrays.asList(names));
+		}
+		
+	}
+
+	public List<String> getNames() {
+		return names;
+	}
+
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+	
+	public String toString() {
+		Iterator<String> itr = this.names.iterator();
+		String tmp = "";
+		while(itr.hasNext()) {
+			tmp = tmp + itr.next() + ", ";
+		}
+		return tmp;
+	}
+	
+	public void rotate(int distance) {
+		Collections.rotate(this.names, distance);
+	}
+	
+	public void rotateAndRemoveLast(int distamce) {
+		this.rotate(distamce);
+		((LinkedList<String>) this.names).removeLast();
+	}
 
 }
--- src/musicalChairs/MusicalChairsGame.java
+++ src/musicalChairs/MusicalChairsGame.java
@@ -3,7 +3,22 @@ package musicalChairs;
 public class MusicalChairsGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		MusicalChair mc1 = new MusicalChair("Lisa", "Susi", "Tom", "Lukas");
+		System.out.println(mc1);
+		
+		
+		MusicalChair mc2 = new MusicalChair("Madeline", "Benedikt");
+		System.out.println(mc2);
+		
+		//MusicalChair mc3 = new MusicalChair();
+		//System.out.println(mc3);
+		
+		mc1.rotate(2);
+		System.out.println(mc1);
+		
+		mc1.rotateAndRemoveLast(2);
+		System.out.println(mc1);
 
 	}
 
