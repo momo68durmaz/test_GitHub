488bbf1319165055990beb3adc1cc7cbeb414ec0
momo68durmaz <muhammed.durmaz@hak-reutte.ac.at>
2024-04-23 09:10:58 +0200

    .

--- src/Stack/UPN.java
+++ src/Stack/UPN.java
@@ -1,9 +1,44 @@
 package Stack;
 
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UPN {
+	
+	public static int calculateUPN(String input) {
+		LinkedList<Integer> stack = new LinkedList<>();
+		// operatorPattern: überprüft auf mathematische Rechenzeichen
+		Pattern operatorPattern = Pattern.compile("[+*-/]");
+		// numericPattern: \\d...Ziffer [0-9], + beliebig viele Ziffern
+		Pattern numericPattern = Pattern.compile("\\d+");
+		
+		// \\s+ ... teilt den String auch nach mehreren Leerzeichen
+		for(String token : input.split("\\s+")) {
+			Matcher operatorMatcher = operatorPattern.matcher(token);
+			Matcher numericMatcher = numericPattern.matcher(token);
+			if(numericMatcher.matches()) {
+				stack.push(Integer.parseInt(token));
+			} else if(operatorMatcher.matches()) {
+				int operand1 = stack.pop();
+				int operand2 = stack.pop();
+				int result = 0;
+				switch(token) {
+					case "+" : result = operand1 + operand2; break;
+					case "-" : result = operand2 - operand1; break;
+					case "*" : result = operand1 * operand2; break;
+					case "/" : result = operand2 / operand1; break;
+					default: System.out.println("Falscher Operator");
+				}
+				stack.push(result);
+			}
+		}
+		return stack.pop();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "5 3 +";
+		System.out.println(calculateUPN(str));
 
 	}
 
