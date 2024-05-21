3dd504d7f81b2ea0c6e231a0be56b1b2499a0850
momo68durmaz <muhammed.durmaz@hak-reutte.ac.at>
2024-05-21 13:12:17 +0200

    .

--- src/exception/AktuellesDatum.java
+++ src/exception/AktuellesDatum.java
@@ -1,10 +1,21 @@
 package exception;
 
+import java.time.LocalDateTime;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class AktuellesDatum {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+ 
+ 
+    public static void main(String[] args) throws Exception {
+     
+        String fileName = "aktuelles_datum_zeit.txt";
+        try (PrintWriter writer = new PrintWriter(fileName)) {
+        	LocalDateTime now = LocalDateTime.now();
+        	writer.print(now);
+        	
+        } catch(FileNotFoundException e){
+        	System.err.println("Datei konnte nicht geöffnet werden!");
+        }
+    }
 }
